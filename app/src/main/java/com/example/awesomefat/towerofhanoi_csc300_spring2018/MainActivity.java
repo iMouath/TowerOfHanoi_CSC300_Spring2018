d764dc108cbb0bd5adebf271c466f7466363c1d7
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2018-02-21 19:13:38 -0600

    HW #6

--- app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity
     private ViewGroup placeholderVG;
     private Button[] towerButtons = new Button[3];
     private boolean shouldPop = true;
+    private int movesCounter = 0;
+    private TextView movesCounterTV;
+    private ViewGroup tower2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,23 +25,26 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.towerButtons[0] = (Button)this.findViewById(R.id.tower0Button);
-        this.towerButtons[1] = (Button)this.findViewById(R.id.tower1Button);
-        this.towerButtons[2] = (Button)this.findViewById(R.id.tower2Button);
+        this.towerButtons[0] = this.findViewById(R.id.tower0Button);
+        this.towerButtons[1] = this.findViewById(R.id.tower1Button);
+        this.towerButtons[2] = this.findViewById(R.id.tower2Button);
 
-        this.placeholderVG = (ViewGroup)this.findViewById(R.id.placeholderVG);
+        this.placeholderVG = this.findViewById(R.id.placeHolderLL);
 
-        Button disk0 = (Button)this.findViewById(R.id.disk0);
-        Button disk1 = (Button)this.findViewById(R.id.disk1);
-        Button disk2 = (Button)this.findViewById(R.id.disk2);
+        this.movesCounterTV = this.findViewById(R.id.movesCounterTV);
+
+        Button disk0 = this.findViewById(R.id.disk0Small);
+        Button disk1 = this.findViewById(R.id.disk1Medium);
+        Button disk2 = this.findViewById(R.id.disk2Large);
 
         Disk d0 = new Disk(0, disk0);
         Disk d1 = new Disk(1, disk1);
         Disk d2 = new Disk(2, disk2);
 
-        ViewGroup tower0 = (ViewGroup)this.findViewById(R.id.tower0);
-        ViewGroup tower1 = (ViewGroup)this.findViewById(R.id.tower1);
-        ViewGroup tower2 = (ViewGroup)this.findViewById(R.id.tower2);
+        ViewGroup tower0 = this.findViewById(R.id.tower0);
+        ViewGroup tower1 = this.findViewById(R.id.tower1);
+
+        tower2 = this.findViewById(R.id.tower2);
 
         Tower t0 = new Tower(tower0);
         Tower t1 = new Tower(tower1);
@@ -65,6 +73,11 @@ public class MainActivity extends AppCompatActivity
             {
                 b.setText("PUSH");
             }
+            this.movesCounter += 1;
+            this.movesCounterTV.setText("Moves Counter: " + this.movesCounter);
+        }
+        else{
+            Toast.makeText(this, "Illegal Move!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -81,6 +94,11 @@ public class MainActivity extends AppCompatActivity
             {
                 b.setText("POP");
             }
+            this.movesCounter += 1;
+            this.movesCounterTV.setText("Moves Counter: " + this.movesCounter);
+        }
+        else{
+            Toast.makeText(this, "Illegal Move!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -118,5 +136,21 @@ public class MainActivity extends AppCompatActivity
         {
             this.tryToPushToTower(2);
         }
+        if(this.tower2.getChildCount() == 3){
+            Toast.makeText(this, "Game Won!\nMoves: " + this.movesCounter, Toast.LENGTH_LONG).show();
+            for(Button b : this.towerButtons)
+            {
+                b.setEnabled(false);
+                b.setVisibility(View.INVISIBLE);
+            }
+            Button restartBtn = findViewById(R.id.restartBtn);
+            restartBtn.setVisibility(View.VISIBLE);
+            restartBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    MainActivity.super.recreate();
+                }
+            });
+        }
     }
+
 }
--- app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Tower.java
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Tower.java
@@ -1,6 +1,5 @@
 package com.example.awesomefat.towerofhanoi_csc300_spring2018;
 
-import android.view.View;
 import android.view.ViewGroup;
 
 /**
